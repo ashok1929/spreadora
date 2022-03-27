00bccea9a6f319d64987b7e2ce8639a3c5cf1aa9
ashok1929 <asokkumar.t@gmail.com>
2022-03-27 11:39:06 +0530

    sample commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ module.exports = {
 
   /**
    * An asynchronous bootstrap function that runs before
-   * your application gets started.
+   * your application gets started..
    *
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
